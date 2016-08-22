917f6c1b86dd0d197651b334e619749d30ce9e87
yiranyu <767327970@qq.com>
2016-08-22 23:24:58 +0800

    drag

---
+++ basis/js/drag.js
@@ -0,0 +1,40 @@
+﻿/**
+ Implement drag function by javaScript 
+*/
+
+/* Native javaScript */
+function drag(el){
+	var self=this;
+	el.style.left=el.offsetLeft+"px";
+	el.style.top=el.offsetTop+"px";
+	var temp=null,
+		mouseX,
+		mouseY,
+		clientX,
+		clientY,
+		dragging=false;
+	
+	//Initialize parameters when the mouse is down
+	el.onmousedown=function(event){
+		el.style.position="absolute";
+		event=event||window.event;
+		dragging=true;
+		mouseX=event.clientX; 
+		mouseY=event.clientY;
+		clientX=parseInt(el.style.left);
+		clientY=parseInt(el.style.top);
+	}
+	
+	//Reset the coordinate of element when the mouse is moving
+	document.onmousemove=function(event){
+		event=event||window.event;
+		if(dragging){
+			el.style.left=parseInt(event.clientX-mouseX+clientX)+"px";
+			el.style.top=parseInt(event.clientY-mouseY+clientY)+"px";
+		}
+		
+	}
+	document.onmouseup=function(){
+		dragging=false;
+	}
+}
